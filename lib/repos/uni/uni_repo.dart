195ad3ec0af5273b94a1b6153dd8e24afbc133f9
Joshua <joshuaystang@gmail.com>
2020-09-06 15:12:06 +1000

    Add to sort unis and faculties

--- lib/repos/uni/uni_repo.dart
+++ lib/repos/uni/uni_repo.dart
@@ -9,8 +9,15 @@ class UniRepo {
       FirebaseFirestore.instance.collection('unis');
 
   Future<List<Uni>> fetchUnis() async {
-    final snapshot = await _unisRef.get();
-    final unis = snapshot.docs.map((doc) => Uni.fromJson(doc.data())).toList();
+    final snapshot = await _unisRef.orderBy('name').get();
+    final unis = snapshot.docs.map((doc) {
+      final data = doc.data();
+      final faculties = List.from(data['faculties']);
+      faculties.sort();
+      data['faculties'] = faculties;
+
+      return Uni.fromJson(data);
+    }).toList();
 
     return unis;
   }
