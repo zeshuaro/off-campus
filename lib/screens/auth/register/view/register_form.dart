b1ed201b31f0440ceb55c51b5c5b536bbfbfd877
Joshua <joshuaystang@gmail.com>
2020-09-07 16:22:04 +1000

    Remove unnecessary widget

--- lib/screens/auth/register/view/register_form.dart
+++ lib/screens/auth/register/view/register_form.dart
@@ -205,36 +205,34 @@ class _SelectField extends StatelessWidget {
   Widget build(BuildContext context) {
     final borderRadius = BorderRadius.circular(30.0);
 
-    return Material(
-      child: Ink(
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: borderRadius,
-        ),
-        child: InkWell(
-          onTap: onTap,
-          borderRadius: borderRadius,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 12.0,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(label),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      selected ?? 'Select',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    Icon(Icons.arrow_drop_down, color: Colors.grey)
-                  ],
-                )
-              ],
-            ),
+    return Ink(
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: borderRadius,
+      ),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: borderRadius,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 12.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(label),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    selected ?? 'Select',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Icon(Icons.arrow_drop_down, color: Colors.grey)
+                ],
+              )
+            ],
           ),
         ),
       ),
