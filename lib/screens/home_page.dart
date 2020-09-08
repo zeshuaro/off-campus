e2669dd7e3fc6150c4c4f09c751f8bde577e506f
zeshuaro <joshuaystang@gmail.com>
2020-09-08 17:53:31 +1000

    Feature/home page (#9)
    * Update home page and add user card
    
    * Tidy up code
    
    * Remove unused files

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,122 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cache_manager_firebase/flutter_cache_manager_firebase.dart';
+import 'package:offcampus/blocs/auth/auth.dart';
+import 'package:offcampus/common/consts.dart';
+import 'package:offcampus/repos/auth/auth_repo.dart';
+import 'package:offcampus/widgets/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => HomePage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = context.bloc<AuthBloc>().state.user;
+
+    return Scaffold(
+        backgroundColor: kYellow,
+        appBar: AppBar(
+          title: const Text('OffCampus', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.transparent,
+          brightness: Brightness.light,
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.exit_to_app, color: Colors.black),
+              onPressed: () =>
+                  context.bloc<AuthBloc>().add(AuthSignOutRequested()),
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 100.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+            ),
+            Padding(
+              padding: kLayoutPadding,
+              child: Column(
+                children: [_UserCard(user: user)],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class _UserCard extends StatelessWidget {
+  final MyUser user;
+
+  const _UserCard({Key key, @required this.user})
+      : assert(user != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyCard(
+      child: Container(
+        padding: kLayoutPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user.name,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      WidgetPaddingSm(),
+                      Text(
+                        user.university,
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 16.0,
+                ),
+                ClipOval(
+                  child: CachedNetworkImage(
+                    cacheManager: FirebaseCacheManager(),
+                    width: 100.0,
+                    height: 100.0,
+                    imageUrl: user.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+            WidgetPadding(),
+            Align(
+              alignment: Alignment.center,
+              child: Text(user.degree),
+            ),
+            WidgetPadding(),
+            Text(
+              'About Me',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text(user.summary),
+          ],
+        ),
+      ),
+    );
+  }
+}
