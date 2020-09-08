71e674fb0dd0a4bf13f637304677a56e564bf642
Joshua <joshuaystang@gmail.com>
2020-09-08 18:05:05 +1000

    Remove option to skip complete profile

--- lib/screens/complete_profile/view/complete_profile_page.dart
+++ lib/screens/complete_profile/view/complete_profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:offcampus/common/consts.dart';
 import 'package:offcampus/screens/complete_profile/view/complete_profile_form.dart';
-import 'package:offcampus/screens/home/home.dart';
 
 class CompleteProfilePage extends StatelessWidget {
   static Route route() => MaterialPageRoute(
@@ -17,15 +16,6 @@ class CompleteProfilePage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         brightness: Brightness.light,
         elevation: 0.0,
-        actions: [
-          FlatButton(
-            onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-              HomePage.route(),
-              (route) => false,
-            ),
-            child: Text('Skip'),
-          ),
-        ],
       ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
