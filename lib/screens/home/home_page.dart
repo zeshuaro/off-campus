ed4aef7f81208a3d48fc6c27fadc6224c11029bf
Joshua <joshuaystang@gmail.com>
2020-09-11 09:45:51 +1000

    Tidy up code

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -66,90 +66,125 @@ class _UserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-
     return SlimyCard(
       color: Colors.amber[200],
-      width: width,
+      width: MediaQuery.of(context).size.width,
       topCardHeight: 420,
       bottomCardHeight: 150,
       borderRadius: 15,
-      topCardWidget: Column(
-        children: [
-          WidgetPadding(),
-          ClipOval(
-            child: CachedNetworkImage(
-              cacheManager: FirebaseCacheManager(),
-              width: 150.0,
-              height: 150.0,
-              imageUrl: user.image,
-              fit: BoxFit.cover,
-            ),
-          ),
-          WidgetPadding(),
-          Text(
-            user.name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                .apply(color: Colors.black, fontWeightDelta: 2),
-          ),
-          WidgetPaddingSm(),
-          Text(
-            user.degree,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          WidgetPaddingSm(),
-          Text(
-            user.university,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          WidgetPaddingSm(),
-          RaisedButton(
-            onPressed: () {
-              final currUser = context.bloc<AuthBloc>().state.user;
-              final chatId = currUser.id + user.id;
-              final chat = Chat(
-                id: chatId,
-                type: ChatType.private,
-                userIds: <String>[currUser.id, user.id],
-                title: user.name,
-                isInit: true,
-              );
+      topCardWidget: _TopCardWidget(user: user),
+      bottomCardWidget: _BottomCardWidget(summary: user.summary),
+    );
+  }
+}
 
-              Navigator.of(context).push(ChatPage.route(chat));
-            },
-            color: Theme.of(context).primaryColor,
-            shape: StadiumBorder(),
-            child: Text(
-              'Send Message',
-              style: TextStyle(color: Colors.white),
-            ),
+class _TopCardWidget extends StatelessWidget {
+  final MyUser user;
+
+  const _TopCardWidget({Key key, @required this.user})
+      : assert(user != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        WidgetPadding(),
+        ClipOval(
+          child: CachedNetworkImage(
+            cacheManager: FirebaseCacheManager(),
+            width: 150.0,
+            height: 150.0,
+            imageUrl: user.image,
+            fit: BoxFit.cover,
           ),
-        ],
-      ),
-      bottomCardWidget: SizedBox(
-        width: width,
-        child: ListView(
-          padding: kLayoutPadding,
-          children: [
-            Text(
-              'About Me',
-              style: Theme.of(context)
-                  .textTheme
-                  .subtitle1
-                  .apply(fontWeightDelta: 1),
-            ),
-            Text(user.summary),
-          ],
         ),
+        WidgetPadding(),
+        Text(
+          user.name,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context)
+              .textTheme
+              .headline5
+              .apply(color: Colors.black, fontWeightDelta: 2),
+        ),
+        WidgetPaddingSm(),
+        Text(
+          user.degree,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        WidgetPaddingSm(),
+        Text(
+          user.university,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        WidgetPaddingSm(),
+        _SendMessageButton(user: user),
+      ],
+    );
+  }
+}
+
+class _SendMessageButton extends StatelessWidget {
+  final MyUser user;
+
+  const _SendMessageButton({Key key, @required this.user})
+      : assert(user != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        final currUser = context.bloc<AuthBloc>().state.user;
+        final chatId = currUser.id + user.id;
+        final chat = Chat(
+          id: chatId,
+          type: ChatType.private,
+          userIds: <String>[currUser.id, user.id],
+          title: user.name,
+          isInit: true,
+        );
+
+        Navigator.of(context).push(ChatPage.route(chat));
+      },
+      color: Theme.of(context).primaryColor,
+      shape: StadiumBorder(),
+      child: Text(
+        'Send Message',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
+
+class _BottomCardWidget extends StatelessWidget {
+  final String summary;
+
+  const _BottomCardWidget({Key key, @required this.summary})
+      : assert(summary != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ListView(
+        padding: kLayoutPadding,
+        children: [
+          Text(
+            'About Me',
+            style:
+                Theme.of(context).textTheme.subtitle1.apply(fontWeightDelta: 1),
+          ),
+          Text(summary),
+        ],
       ),
     );
   }
