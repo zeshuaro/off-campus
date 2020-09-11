fbc89d4b8c19c664b1c31efe3bfe2b75cf97212d
Joshua <joshuaystang@gmail.com>
2020-09-11 10:04:35 +1000

    Fix complete profile layout

--- lib/screens/complete_profile/view/complete_profile_form.dart
+++ lib/screens/complete_profile/view/complete_profile_form.dart
@@ -26,27 +26,22 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        child: MyCard(
-          child: Padding(
-            padding: kLayoutPadding,
-            child: Column(
-              children: [
-                Text('Add a profile picture'),
-                WidgetPadding(),
-                _buildSelectImage(),
-                WidgetPadding(),
-                _buildTextField(),
-                WidgetPadding(),
-                SizedBox(
-                    height: 36.0,
-                    child: _isLoading
-                        ? CircularProgressIndicator()
-                        : _buildButton()),
-              ],
-            ),
-          ),
+    return MyCard(
+      child: Padding(
+        padding: kLayoutPadding,
+        child: Column(
+          children: [
+            Text('Add a profile picture'),
+            WidgetPadding(),
+            _buildSelectImage(),
+            WidgetPadding(),
+            _buildTextField(),
+            WidgetPadding(),
+            SizedBox(
+                height: 36.0,
+                child:
+                    _isLoading ? CircularProgressIndicator() : _buildButton()),
+          ],
         ),
       ),
     );
